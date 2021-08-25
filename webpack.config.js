e7ac37e051cc62c26fc332d091323317145b7e9e
jack <jackjack5810@yahoo.com>
2021-08-25 11:38:07 -0400

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,19 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require("path");
 const bundleOutputDir = "./dist";
 
 module.exports = {
     entry: {
-        main: "./src/main"  
+        main: "./src/ts/main"  
     },
     output: {
         filename: "[name].bundle.js",
         path: path.join(__dirname, bundleOutputDir),
-        publicPath: 'public/dist/'
     },
     devtool: "source-map",
     resolve: {
-        extensions: ['.js', '.ts']
+        extensions: ['.js', '.ts', 'tsx']
     },
     module: {
         rules: [
@@ -22,11 +23,23 @@ module.exports = {
             },            
             { test: /\.tsx?$/, loader: "ts-loader" },        
             {
-                test: /\.css$/,
-                sideEffects: true,
-                loader: "css-loader"
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader"]
+            },
+            {
+                test: /\.html$/,
+                include: [path.resolve(__dirname, 'src')],
+                use: ['html-loader']
             }
         ]
-    }
+    },
+    plugins: [
+        new MiniCssExtractPlugin(),
+        new HtmlWebpackPlugin({
+          template: './src/index.html',
+          inject: 'head',
+          filename: 'index.html'
+        })
+      ]
 };
 
