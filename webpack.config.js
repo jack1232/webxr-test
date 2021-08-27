370bdc2cc90a0e3aa8903501445c97e980a0594b
jack <jackjack5810@yahoo.com>
2021-08-27 14:21:00 -0400

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 const path = require("path");
 const bundleOutputDir = "./dist";
 
@@ -28,8 +29,16 @@ module.exports = {
             },
             {
                 test: /\.html$/,
-                include: [path.resolve(__dirname, 'src')],
-                use: ['html-loader']
+                use: [{
+                    loader: 'html-loader'
+                }]
+            },
+            {
+                test: /\.(jpe?g|png|gif)$/,
+                type: 'asset/resource',
+                generator: {
+                filename: 'assets/[hash][ext]'
+                }
             }
         ]
     },
